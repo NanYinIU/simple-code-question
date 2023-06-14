7745b2ec7cf89dff93100c5feb5766e4a9c93299
nanyiniu <1977713379@qq.com>
2023-06-14 14:44:29 +0800

    init

---
+++ src/main/java/com/simple/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,25 @@
+package com.simple.leetcode.editor.cn;
+
+/**
+ * Description:
+ *
+ * @author: gaoguoxing
+ * @date: 2023/6/14 11:52
+ * @version: 1.0
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
